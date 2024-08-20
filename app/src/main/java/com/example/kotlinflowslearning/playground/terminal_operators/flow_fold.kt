7df9e448f2038156aa6aede2add626a9cae1410e
Nicolas Escobar Cruz <nicoescobar69@hotmail.com>
2024-08-20 15:22:25 -0600

    Implement flow fold operator

---
+++ app/src/main/java/com/example/kotlinflowslearning/playground/terminal_operators/flow_fold.kt
@@ -0,0 +1,30 @@
+package com.example.kotlinflowslearning.playground.terminal_operators
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.fold
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.flow.toSet
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+
+    val flow = flow {
+        delay(100)
+
+        println("Emitting first value")
+        emit(1)
+
+        delay(100)
+
+        println("Emitting second value")
+        emit(2)
+    }
+
+    runBlocking {
+        val item = flow.fold(5) { accumulator, emittedItem ->
+            accumulator + emittedItem
+        }
+        println("Received item $item")
+    }
+}
