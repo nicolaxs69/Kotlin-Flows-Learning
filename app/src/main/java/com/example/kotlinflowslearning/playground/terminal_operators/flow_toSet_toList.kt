b176b85720e6af22b41a9534cafca74b59a74f97
Nicolas Escobar Cruz <nicoescobar69@hotmail.com>
2024-08-20 15:23:40 -0600

    Review the differences between toSet() and toList()

---
+++ app/src/main/java/com/example/kotlinflowslearning/playground/terminal_operators/flow_toSet_toList.kt
@@ -0,0 +1,30 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.flow.toSet
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+
+    val flow = flow {
+        delay(100)
+
+        println("Emitting first value")
+        emit(1)
+
+        delay(100)
+
+        println("Emitting second value")
+        emit(2)
+    }
+
+    runBlocking {
+        val item = flow.toSet()
+        println("Received item $item")
+    }
+
+    runBlocking {
+        val item = flow.toList()
+        println("Received item $item")
+    }
+}
